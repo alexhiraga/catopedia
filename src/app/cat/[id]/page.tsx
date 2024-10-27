1e685bf07535b0b1d39f7c319d3cceb68c746341
Alex Hiraga <105091895+alexhiraga@users.noreply.github.com>
2024-10-27 18:00:01 -0300

    debugging

--- src/app/cat/[id]/page.tsx
+++ src/app/cat/[id]/page.tsx
@@ -1,11 +1,12 @@
 import CatDetails from "./CatDetails";
 
 export async function generateStaticParams() {
+  console.log("entrou generateStaticarams")
   return [{ id: '1' }]
 }  
 
 export default function CatPage() {
-
+  console.log("entrou catPage")
   return <CatDetails />
  
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@ import MainGrid from "./components/grid/MainGrid";
 import Profile from "./components/profile/Profile";
 
 export default function Home() {
-  console.log('aaa', process.env.NEXT_PUBLIC_CAT_X_API_KEY)
-
   return (
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 xl:max-w-screen-xl w-full">
       <Profile />
