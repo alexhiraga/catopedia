c39adde8de7dbd27101bdea5f8596355823da950
Alex Hiraga <105091895+alexhiraga@users.noreply.github.com>
2024-10-27 18:17:53 -0300

    debugging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import MainGrid from "./components/grid/MainGrid";
 import Profile from "./components/profile/Profile";
 
 export default function Home() {
-  console.log('process.env.NEXT_PUBLIC_OUTPUT', process.env.NEXT_PUBLIC_OUTPUT)
   return (
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 xl:max-w-screen-xl w-full">
       <Profile />
