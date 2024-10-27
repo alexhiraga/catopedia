7ad6b99255117dbc0372e32512743da46dd6bacc
Alex Hiraga <105091895+alexhiraga@users.noreply.github.com>
2024-10-27 17:43:20 -0300

    testing api key consolelog

--- src/api/axiosConfig.tsx
+++ src/api/axiosConfig.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+console.log('aaa', process.env.NEXT_PUBLIC_CAT_X_API_KEY)
 const axiosInstance = axios.create({
   baseURL: 'https://api.thecatapi.com/v1',
   headers: {
