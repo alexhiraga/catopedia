7ca51ee895fad7088cfb55d355ec194083b2a7a9
Alex Hiraga <105091895+alexhiraga@users.noreply.github.com>
2024-10-28 00:26:19 -0300

    add generateStaticParams

--- src/app/cat/[id]/page.tsx
+++ src/app/cat/[id]/page.tsx
@@ -1,5 +1,9 @@
 import CatDetails from "./CatDetails";
 
+export async function generateStaticParams() {
+  return [{id: '1'}]
+}
+
 export default async function CatPage({ params }: { params: Promise<{ id: string }> }) {
   const resolvedParams = await params;
   return <CatDetails id={resolvedParams.id} />;
